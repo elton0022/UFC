6028702103b42d78f7c9e3e37bf0af0e13aa7d0f
Elton Rodrigues <31295927+elton0022@users.noreply.github.com>
2019-08-13 14:31:58 -0300

    new commit

---
+++ Inteligência Artificial/Lista - Python/Parte4.py
@@ -0,0 +1,17 @@
+#--------------Variáveis, expressões e funções de entrada e saída -------------
+
+# Exercício 1
+
+celsius = float(input("Digite a temperatura: "))
+f = (celsius * 1.8) + 32
+print('Fahrenheit: ', f)
+
+#Exercício 2
+
+f = float(input("Digite a temperatura: "))
+cel = (f - 32) * 5/9
+print('Celsius: ',cel)
+
+
+
+
